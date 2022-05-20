0c990481f83c2074e15179263f59e45449ef6eae
lancelot1926 <es.huluca@gmail.com>
2022-05-21 01:44:05 +0300

    Basic Movement Done

--- Top Guns/Assets/PlayerMovement.cs
+++ Top Guns/Assets/PlayerMovement.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Rigidbody2D rigBody;
+    [SerializeField]
+    private Animator animator;
+    private Vector3 moveDir;
+
+    private float moveSpeed = 80f;
+    public float moveX;
+    public float moveY;
+    public float mouseX;
+    public float mouseY;
+
+    private void Awake()
+    {
+        
+    }
+
+
+
     void Start()
     {
         
@@ -13,6 +31,51 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveX = 0f;
+        moveY = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 1);
+            moveY = +1f;
+            
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += new Vector3(0, -1);
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position += new Vector3(-1, 0);
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position += new Vector3(1, 0);
+            moveX = +1f;
+        }
+        moveDir = new Vector3(moveX, moveY).normalized;
+        bool isIdle = moveX == 0 && moveY == 0;
+
+        if (isIdle)
+        {
+            rigBody.velocity = Vector2.zero;
+            animator.SetBool("Walking", false);
+            //animator.SetFloat("XMovement", moveDir.x);
+            //animator.SetFloat("YMovement", moveDir.y);
+        }
+
+        if (isIdle == false)
+        {
+            animator.SetBool("Walking", true);
+            animator.SetFloat("XMovement", moveDir.x);
+            animator.SetFloat("YMovement", moveDir.y);
+        }
         
+    }
+
+    private void FixedUpdate()
+    {
+        rigBody.velocity = moveDir*moveSpeed;
     }
 }

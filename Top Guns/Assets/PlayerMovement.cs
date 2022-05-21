17e8c602092df61569fbfc33980ca09628ed7a4a
lancelot1926 <es.huluca@gmail.com>
2022-05-21 15:29:21 +0300

    More Animations And Aim Done

--- Top Guns/Assets/PlayerMovement.cs
+++ Top Guns/Assets/PlayerMovement.cs
@@ -9,8 +9,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Animator animator;
     private Vector3 moveDir;
-
-    private float moveSpeed = 80f;
+    private Vector2 mousePos;
+    private Vector2 lookDir;
+    public Camera cam;
+    private float moveSpeed = 10f;
     public float moveX;
     public float moveY;
     public float mouseX;
@@ -31,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Shooting", false);
         moveX = 0f;
         moveY = 0f;
         if (Input.GetKey(KeyCode.W))
@@ -54,7 +57,15 @@ public class PlayerMovement : MonoBehaviour
             //transform.position += new Vector3(1, 0);
             moveX = +1f;
         }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            animator.SetBool("Shooting", true);
+        }
+
+        //Debug.Log(animator.GetBool("Shooting"));
+        Debug.Log(mousePos);
         moveDir = new Vector3(moveX, moveY).normalized;
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         bool isIdle = moveX == 0 && moveY == 0;
 
         if (isIdle)
@@ -71,11 +82,19 @@ public class PlayerMovement : MonoBehaviour
             animator.SetFloat("XMovement", moveDir.x);
             animator.SetFloat("YMovement", moveDir.y);
         }
+        if (animator.GetBool("Shooting") == true)
+        {
+            animator.SetFloat("CursorX", mousePos.x);
+            animator.SetFloat("CursorY", mousePos.y);
+        }
+
         
     }
 
     private void FixedUpdate()
     {
         rigBody.velocity = moveDir*moveSpeed;
+
+        lookDir = mousePos - rigBody.position;
     }
 }

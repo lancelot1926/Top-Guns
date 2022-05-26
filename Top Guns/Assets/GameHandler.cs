98b0878fcb9fc334da198c16b14bdd68bf4a7ab4
lancelot1926 <es.huluca@gmail.com>
2022-05-26 12:36:31 +0300

    more map and some coding done

---
+++ Top Guns/Assets/GameHandler.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> SpawnLoc;
+    [SerializeField]
+    private GameObject NormalSoldier;
+    
+    [SerializeField]
+    private List<GameObject> SpwanedSoldierList;
+    
+    public int LevelCounter;
+    public int PointCounter;
+    public int EnemyCounter;
+    private enum State
+    {
+        StartOfTheLevel,
+        EndOfTheLevel,
+        InLevel,
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        EnemyCounter = SpwanedSoldierList.Count;
+
+
+    }
+
+    private void SpawnEnemy() 
+    {
+        switch (LevelCounter)
+        {
+            case 1:
+                //2
+                for(int x = 0; x< 2; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+            case 2:
+                //5
+                for (int x = 0; x < 5; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+            case 3:
+                //8
+                for (int x = 0; x < 8; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+            case 4:
+                //11
+                for (int x = 0; x < 11; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+            case 5:
+                //15
+                for (int x = 0; x < 15; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+            case 6:
+                //20
+                for (int x = 0; x < 20; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+            case 7:
+                //25
+                for (int x = 0; x < 25; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+            case 8:
+                //30
+                for (int x = 0; x < 30; x++)
+                {
+                    GameObject spawnedSoldier = Instantiate(NormalSoldier, SpawnLoc[x]);
+                    SpwanedSoldierList.Add(spawnedSoldier);
+                }
+                break;
+        }
+    }
+}
---
+++ Top Guns/Assets/UnitStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public int CurrentHealth;
+    public int MaxHealth;
+    public GameObject HealthBar;
+    private GameHandler gameHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameHandler = GameObject.Find("GameHandler").GetComponent<GameHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+    }
+
+    public void RecieveHealth(int health)
+    {
+        CurrentHealth += health;
+        if (CurrentHealth>MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+    }
+    private void HealthCheck()
+    {
+        if (CurrentHealth <= 0)
+        {
+            //destroy the unit and remove it from the lists
+        }
+    }
+}
